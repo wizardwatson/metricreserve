fffd8d2faa16399ed4efd11a2ae64d4cb4262d8e
wizardwatson <wizardwatson@gmail.com>
2017-03-27 09:28:03 -0500

    Update metric_reserve.py

--- metric_reserve.py
+++ metric_reserve.py
@@ -6,8 +6,28 @@
 
 
 """
+mlogin:
+	import time
+	import webapp2_extras.appengine.auth.models
+	
+	from google.appengine.ext import ndb
+	
+	from webapp2_extras import security
 
 
+
+	from google.appengine.ext.webapp import template
+	from google.appengine.ext import ndb
+
+	import logging
+	import os.path
+	import webapp2
+
+	from webapp2_extras import auth
+	from webapp2_extras import sessions
+
+	from webapp2_extras.auth import InvalidAuthIdError
+	from webapp2_extras.auth import InvalidPasswordError
  
 # Set your variables here
 email = "someone@somewhere.com"
@@ -28,6 +48,16 @@ import re
 import pickle
 import random
 import bisect
+import time
+import webapp2_extras.appengine.auth.models
+from google.appengine.ext.webapp import template
+import logging
+import os.path
+from webapp2_extras import auth
+from webapp2_extras import sessions
+from webapp2_extras.auth import InvalidAuthIdError
+from webapp2_extras.auth import InvalidPasswordError
+from webapp2_extras import security
 from operator import itemgetter
 # for debugging on dev_appserver.py only
 import pdb
@@ -3405,6 +3435,46 @@ class metric(object):
 ###
 ################################################################
 
+################################################################
+###
+###  BEGIN: mlogin Handler Classes
+###
+################################################################
+
+class User(webapp2_extras.appengine.auth.models.User):
+  def set_password(self, raw_password):
+    """Sets the password for the current user
+    :param raw_password:
+        The raw password which will be hashed and stored
+    """
+    self.password = security.generate_password_hash(raw_password, length=12)
+
+  @classmethod
+  def get_by_auth_token(cls, user_id, token, subject='auth'):
+    """Returns a user object based on a user ID and token.
+    :param user_id:
+        The user_id of the requesting user.
+    :param token:
+        The token string to be verified.
+    :returns:
+        A tuple ``(User, timestamp)``, with a user object and
+        the token timestamp, or ``(None, None)`` if both were not found.
+    """
+    token_key = cls.token_model.get_key(user_id, subject, token)
+    user_key = ndb.Key(cls, user_id)
+    # Use get_multi() to save a RPC call.
+    valid_token, user = ndb.get_multi([token_key, user_key])
+    if valid_token and user:
+        timestamp = int(time.mktime(valid_token.created.timetuple()))
+        return user, timestamp
+
+    return None, None
+
+################################################################
+###
+###  END: mlogin Handler Classes
+###
+################################################################
 
 ################################################################
 ###
