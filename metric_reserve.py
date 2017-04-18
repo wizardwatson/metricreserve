70c64c2e7092cd4ffdf294d7fbc32688b1661497
wizardwatson <wizardwatson@gmail.com>
2017-04-18 11:18:20 -0500

    Update metric_reserve.py

--- metric_reserve.py
+++ metric_reserve.py
@@ -9411,7 +9411,6 @@ class ph_command(webapp2.RequestHandler):
 					search_term = urllib.unquote(lobj_master.request.GET["term"])
 					result_blok["search_result"] = search_term #lobj_master._search(search_term)
 					result_blok["has_search_result"] = True 
-					"""
 
 				bloks.append(self.get_menu_blok())	
 				break
